d9e0d00c86a7571623f2f359bd02dda3e41994fc
mz2166 <mz2820031@gmail.com>
2024-10-05 13:59:02 +0300

    second time

--- app/src/main/java/com/example/example_1/MainActivity.java
+++ app/src/main/java/com/example/example_1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.example_1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+private EditText etwelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        etwelcome=findViewById(R.id.text1);
+
+    }
+
+    public void change_text(View view){
+        etwelcome.setText("this is my first app");
+    }
+    public void finishApp(View view){
+        finish();
+        System.exit(0);
     }
 }
